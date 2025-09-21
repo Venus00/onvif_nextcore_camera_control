f00e0e731e5cfe4a62d52390b9079a5434cdc0c2
venus00 <farhat.oussama00@gmail.com>
2025-09-21 21:17:36 +0100

    fix

--- src/app.ts
+++ src/app.ts
@@ -65,7 +65,7 @@ app.post('/focus/:camId/move', async (req, res) => {
     // stop after 1s (you can adjust)
     setTimeout(() => {
       client.fetch(`http://${ip}/cgi-bin/ptz.cgi?action=stop&channel=${channel}&code=${code}`);
-    }, 1000);
+    }, 10);
 
     res.json({ success: true, camera: camId, response: text });
   } catch (err:any) {
