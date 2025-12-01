1970b81ea3277ce73df033e409ffba67f78dd841
venus00 <farhat.oussama00@gmail.com>
2025-12-01 13:49:58 +0100

    refacto

--- src/app.ts
+++ src/app.ts
@@ -105,7 +105,7 @@ app.post('/ptz/:camId/preset', async (req, res) => {
 
     // Move to preset using CGI configManager API (GotoPreset)
     // Example: http://<ip>/cgi-bin/configManager.cgi?action=setConfig&PtzPreset[0][<preset>].Enable=true
-    const url = `http://${ip}/cgi-bin/configManager.cgi?action=setConfig&PtzPreset[0][1].Name=No1`;
+    const url = `http://${ip}/cgi-bin/action=start&channel=1&code=PositionABSHDX&arg1=1&arg2=0&arg3=0`;
     const response = await client.fetch(url, { method: 'POST' });
     const text = await response.text();
     console.log(text)
